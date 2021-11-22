62d6421d36933cb1cc1b9f68127e7a275d026807
Robin Barling <robin@silverstripe.com>
2021-11-22 14:21:31 +1300

    inject error

--- app/src/PageController.php
+++ app/src/PageController.php
@@ -28,6 +28,9 @@ namespace {
             parent::init();
             // You can include any CSS or JS required by your project here.
             // See: https://docs.silverstripe.org/en/developer_guides/templates/requirements/
+
+
+            echo "test"
         }
     }
 }
