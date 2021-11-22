50f520e82444d8c308569f80bbfe9d046719172c
Robin Barling <robin@silverstripe.com>
2021-11-22 17:12:40 +1300

    Hopefully can set app version in Raygun

--- app/_config.php
+++ app/_config.php
@@ -7,3 +7,6 @@ use SilverStripe\Security\Member;
 $validator = PasswordValidator::create();
 // Settings are registered via Injector configuration - see passwords.yml in framework
 Member::set_password_validator($validator);
+
+$client = Injector::inst()->get(Raygun4php\RaygunClient::class);
+$client->SetVersion('123456');
