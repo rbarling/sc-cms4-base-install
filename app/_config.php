9c914c8c6dba1ab3764b31c25964dcb3560988a0
Robin Barling <robin@silverstripe.com>
2021-11-23 08:22:36 +1300

    Set version using yml

--- app/_config.php
+++ app/_config.php
@@ -3,12 +3,12 @@
 use SilverStripe\Security\PasswordValidator;
 use SilverStripe\Security\Member;
 
-use SilverStripe\Core\Injector\Injector;
+//use SilverStripe\Core\Injector\Injector;
 
 // remove PasswordValidator for SilverStripe 5.0
 $validator = PasswordValidator::create();
 // Settings are registered via Injector configuration - see passwords.yml in framework
 Member::set_password_validator($validator);
 
-$client = Injector::inst()->get(Raygun4php\RaygunClient::class);
-$client->SetVersion('123456');
+//$client = Injector::inst()->get(Raygun4php\RaygunClient::class);
+//$client->SetVersion('123456');
