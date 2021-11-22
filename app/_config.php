bb4a51b674964e2cc4943cf5da5d99a3424934c5
Robin Barling <robin@silverstripe.com>
2021-11-23 07:28:02 +1300

    Fix injector

--- app/_config.php
+++ app/_config.php
@@ -3,6 +3,8 @@
 use SilverStripe\Security\PasswordValidator;
 use SilverStripe\Security\Member;
 
+use SilverStripe\Core\Injector\Injector;
+
 // remove PasswordValidator for SilverStripe 5.0
 $validator = PasswordValidator::create();
 // Settings are registered via Injector configuration - see passwords.yml in framework
